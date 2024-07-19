9e65e2d9b504b0d96741e27732975b186323e3af
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-07-20 00:23:09 +0800

    refactor: C#新项目 Issue [重构] 可以用C#重写下，效率更高 #20

---
+++ C#/中文git/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace 中文git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Code here.
+            Environment.ExitCode = 0;
+        }
+    }
+}
