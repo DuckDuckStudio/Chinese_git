7ccd22c72c6c1776ca9dc7d5494968136c3c44c2
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-07-20 15:58:54 +0800

    fix: 修复iss文件多余空格的问题

--- AutoPack/AutoPack.py
+++ AutoPack/AutoPack.py
@@ -116,39 +116,39 @@ def generate_iss_file():
     if releases_version.startswith('v'):
         iss_version = releases_version[1:]
     iss_file = '''[Setup]
-    AppName=中文Git
-    #define cngitversion "程序版本"
-    AppVersion=v{#cngitversion}
-    VersionInfoVersion={#cngitversion}
-    AppPublisher=DuckStudio
-    VersionInfoCopyright=Copyright (c) 鸭鸭「カモ」
-    AppPublisherURL=https://duckduckstudio.github.io/yazicbs.github.io/Tools/chinese_git/
-    DefaultDirName={autopf}\\Chinese_git
-    DefaultGroupName=中文Git
-    UninstallDisplayIcon={app}\\中文git.exe
-    OutputDir=D:\\Duckhome\\projects\\MSVS\\Source\\Repos\\Chinese_git\\AutoPack\\Releases\\v{#cngitversion}
-    OutputBaseFilename=Chinese_git_Setup_v{#cngitversion}
-    SetupIconFile=D:\\Duckhome\\projects\\MSVS\\Source\\Repos\\Chinese_git\\ico.ico
-    LicenseFile=D:\\Duckhome\\projects\\MSVS\\Source\\Repos\\Chinese_git\\发行版\\LICENSE
-    Compression=lzma2
-    SolidCompression=yes
+AppName=中文Git
+#define cngitversion "程序版本"
+AppVersion=v{#cngitversion}
+VersionInfoVersion={#cngitversion}
+AppPublisher=DuckStudio
+VersionInfoCopyright=Copyright (c) 鸭鸭「カモ」
+AppPublisherURL=https://duckduckstudio.github.io/yazicbs.github.io/Tools/chinese_git/
+DefaultDirName={autopf}\\Chinese_git
+DefaultGroupName=中文Git
+UninstallDisplayIcon={app}\\中文git.exe
+OutputDir=D:\\Duckhome\\projects\\MSVS\\Source\\Repos\\Chinese_git\\AutoPack\\Releases\\v{#cngitversion}
+OutputBaseFilename=Chinese_git_Setup_v{#cngitversion}
+SetupIconFile=D:\\Duckhome\\projects\\MSVS\\Source\\Repos\\Chinese_git\\ico.ico
+LicenseFile=D:\\Duckhome\\projects\\MSVS\\Source\\Repos\\Chinese_git\\发行版\\LICENSE
+Compression=lzma2
+SolidCompression=yes
 
-    [Languages]
-    Name: "english"; MessagesFile: "compiler:Default.isl"
-    Name: "chinesesimplified"; MessagesFile: "compiler:Languages\\ChineseSimplified.isl"
-    Name: "japanese"; MessagesFile: "compiler:Languages\\Japanese.isl"
+[Languages]
+Name: "english"; MessagesFile: "compiler:Default.isl"
+Name: "chinesesimplified"; MessagesFile: "compiler:Languages\\ChineseSimplified.isl"
+Name: "japanese"; MessagesFile: "compiler:Languages\\Japanese.isl"
 
-    [Files]
-    Source: "D:\\Duckhome\\projects\\MSVS\\Source\\Repos\\Chinese_git\\AutoPack\\Releases\\v{#cngitversion}\\Chinese_git\\*"; DestDir: "{app}"
+[Files]
+Source: "D:\\Duckhome\\projects\\MSVS\\Source\\Repos\\Chinese_git\\AutoPack\\Releases\\v{#cngitversion}\\Chinese_git\\*"; DestDir: "{app}"
 
-    [Icons]
-    Name: "{group}\\中文Git"; Filename: "{app}\\中文git.exe"
+[Icons]
+Name: "{group}\\中文Git"; Filename: "{app}\\中文git.exe"
 
-    [Run]
-    Filename: "{sys}\\cmd.exe"; Parameters: "/C setx PATH ""{app};%PATH%"" /M"; Flags: runhidden
+[Run]
+Filename: "{sys}\\cmd.exe"; Parameters: "/C setx PATH ""{app};%PATH%"" /M"; Flags: runhidden
 
-    [UninstallRun]
-    Filename: "{sys}\\cmd.exe"; Parameters: "/C setx PATH ""%PATH:{app};=%"" /M"; Flags: runhidden; RunOnceId: UninstallSetPath
+[UninstallRun]
+Filename: "{sys}\\cmd.exe"; Parameters: "/C setx PATH ""%PATH:{app};=%"" /M"; Flags: runhidden; RunOnceId: UninstallSetPath
     '''
     iss_file = iss_file.replace("程序版本", iss_version)
     try:
