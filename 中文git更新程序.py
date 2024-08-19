d27b4ed95079385fceea03a448e8376c20a6a40f
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-08-19 15:28:00 +0800

    fix: 修复异常返回错误代码
    feat: 更新程序可以返回正常/错误代码
    fix: 修改自动打包流触发器，避免重复触发

--- 中文git更新程序.py
+++ 中文git更新程序.py
@@ -6,7 +6,10 @@ from colorama import init, Fore
 
 init(autoreset=True)
 
+exit_code = 0
+
 def download_update_file(version):
+    global exit_code
     # 根据版本确定下载 URL
     download_url = f'https://github.com/DuckDuckStudio/Chinese_git/releases/download/{version}/Chinese_git.exe'
     #spare_download_url = f'https://duckduckstudio.github.io/yazicbs.github.io/Tools/chinese_git/Spare-Download/Chinese_git.exe'
@@ -41,15 +44,18 @@ def download_update_file(version):
         #    except Exception as e:
         #        print(f"{Fore.RED}✕{Fore.RESET} 下载更新文件时出错: {e}")
         #        return None
+        exit_code = 1
         return None
     
 def replace_current_program(new_filename):
+    global exit_code
     try:
         # 用下载的文件替换当前程序
         os.replace(new_filename, os.path.join(os.path.dirname(sys.argv[0]), "中文git.exe"))
         print(f"{Fore.GREEN}✓{Fore.RESET} 程序已成功更新。")
     except Exception as e:
         print(f"{Fore.RED}✕{Fore.RESET} 替换当前程序时出错: {e}")
+        exit_code = 1
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="打包版中文Git的更新程序")
@@ -61,3 +67,4 @@ if __name__ == "__main__":
         replace_current_program(new_filename)
 
     input(f"按{Fore.BLUE}Enter{Fore.RESET}键退出...")
+sys.exit(exit_code)
