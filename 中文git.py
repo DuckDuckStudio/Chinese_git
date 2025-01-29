360e118cb84706a169b1a12d0b8410c91a5f0097
鸭鸭「カモ」 <Yzcbs123@163.com>
2025-01-30 00:56:49 +0800

    chore(ver): Bump version from 3.0 to 3.1

--- 中文git.py
+++ 中文git.py
@@ -12,7 +12,7 @@ exit_code = 0 # 只有不正常退出需要定义
 
 # ---------- 版本定义及更新 ----------
 # 定义版本号
-VERSION = 'v3.0'
+VERSION = 'v3.1'
 # GitHub releases API URL
 url = 'https://api.github.com/repos/DuckDuckStudio/Chinese_git/releases/latest'
 
